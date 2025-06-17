5d535eb949424d369b332949b2e00e8756c32a64
Halini <nurhalini.nbk2204@gmail.com>
2025-06-17 20:37:25 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,9 +17,32 @@
         // Part Two: Implement the RemoveItem method
         //Status: Done
         manager.RemoveItem("Apple");
-        // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
-        // TODO: Implement this part four.
-        //Interface
+        // Part Three: Introduce a Fruit class and use the ItemManager<Fruit> to add a few fruits and print them on the console.
+        // TODO: Implement this part three.
+        //Status
+
+        ItemManager<Fruit> fruitList = new ItemManager<Fruit> { };
+        int NumberOfFruitsToAdd = 0;
+        System.Console.WriteLine("How many fruits would you like to add?");
+        NumberOfFruitsToAdd = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < NumberOfFruitsToAdd; i++)
+        {
+            Console.WriteLine("What fruit would you like to add: ");
+            String? fruitName = Console.ReadLine();
+            if (!String.IsNullOrEmpty(fruitName))
+            {
+                  Fruit f = new Fruit(fruitName);
+                  fruitList.AddItem(f);
+            }
+            else
+            {
+                  Console.WriteLine("No input for the fruit name.");
+            }
+       }
+       fruitList.PrintAllItems();
+       // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
+       // TODO: Implement this part four.
+       //Interface
     }
 }
 // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
